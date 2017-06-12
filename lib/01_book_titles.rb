5f1689f372c853ea249bd4a33c52b4e1194a4a96
Mike Salisbury <michael.salisbury87@gmail.com>
2017-06-11 23:31:53 -0700

    Completed first 2 specs

--- lib/01_book_titles.rb
+++ lib/01_book_titles.rb
@@ -1,3 +1,17 @@
 class Book
-  # TODO: your code goes here!
+attr_accessor :title
+
+  def title
+    non_cap = %w(the of an in a and or)
+
+    book_title = @title.split(" ").map.with_index do |word, idx|
+      if non_cap.include?(word) && idx == 0 || non_cap.include?(word) == false
+        word.capitalize
+      else
+        word
+      end
+    end
+
+  book_title.join(" ")
+  end
 end
