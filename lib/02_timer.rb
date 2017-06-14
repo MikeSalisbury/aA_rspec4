b18cd2139c62091416d5b77b9c189275f8e9fac8
Mike Salisbury <michael.salisbury87@gmail.com>
2017-06-13 23:33:10 -0700

    completed exercise 03_temperature_object

--- lib/02_timer.rb
+++ lib/02_timer.rb
@@ -1,3 +1,37 @@
 class Timer
+  attr_accessor :seconds
+
+  def initialize(seconds = 0)
+    @seconds = seconds
+  end
+
+  def time_string
+    hours = 0
+    minutes = 0
+
+    while @seconds >= 60
+      minutes += 1
+      @seconds -= 60
+    end
+
+    while minutes >= 60
+      hours += 1
+      minutes -= 60
+    end
+
+    if hours < 10
+      hours = "0#{hours}"
+    end
+
+    if minutes < 10
+      minutes = "0#{minutes}"
+    end
+
+    if @seconds < 10
+      @seconds = "0#{@seconds}"
+    end
+
+    "#{hours}:#{minutes}:#{@seconds}"
+  end
 
 end
--- lib/03_temperature_object.rb
+++ lib/03_temperature_object.rb
@@ -1,3 +1,43 @@
 class Temperature
-  # TODO: your code goes here!
+
+  def initialize(option = {})
+    @option = option
+  end
+
+  def in_fahrenheit
+    if @option.keys[0] == :c
+      return @option.values[0] * 9/5.0 + 32
+    else
+      return @option.values[0]
+    end
+  end
+
+  def in_celsius
+    if @option.keys[0] == :f
+      return (@option.values[0] - 32) * (5/9.0)
+    else
+      return @option.values[0]
+    end
+  end
+
+  def self.from_fahrenheit(temp)
+    self.new(f: temp)
+  end
+
+  def self.from_celsius(temp)
+    self.new(c: temp)
+  end
 end
+
+  class Celsius < Temperature
+    def initialize(temp)
+      super({c: temp})
+    end
+
+  end
+
+  class Fahrenheit < Temperature
+    def initialize(temp)
+      super({f: temp})
+    end
+  end
